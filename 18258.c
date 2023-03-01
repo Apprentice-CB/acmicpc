6453ec9a6b87618ed8200232da5d7e54d26d0aa3
Apprentice-CB <yewon0612@gmail.com>
2023-03-01 22:23:29 +0900

    18258 solved, gitignore added

--- 18258.c
+++ 18258.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
-typedef struct _Node
+typedef struct node
 {
     int value;
     struct node* next;
 } node;
 
-typedef struct _Queue
+typedef struct queue
 {
     struct node* first;
     struct node* last;
@@ -19,30 +20,43 @@ void init(queue* que)
 {
     que->first = NULL;
     que->last = NULL;
+    que->size = 0;
 }
 
 void push(queue* que, int newData)
 {  
     node* newNode;
-    newNode = malloc(sizeof(node));
-    if (que->first == NULL)
+    newNode = (node*)malloc(sizeof(node));
+    newNode->value = newData;
+    newNode->next = NULL;
+    if ((que->first == NULL))
     {
         que->first = newNode;
         que->last = newNode;
     }
-    newNode->next = que->last;
-    newNode->value = newData;
-    que->last = newNode;
+    else
+    {
+        que->last->next = newNode;
+        que->last = newNode;
+    }
     que->size++;
 }
 
 void pop(queue* que)
 {
-    node* first = que->first;
-    printf("%d", first->value);
-    que->first = first->next;
-    free(que->first);
-    que->size--;
+    if (que->first == NULL)
+    {
+        printf("-1\n");
+        return;
+    }
+    else 
+    {
+        node* first = que->first;
+        printf("%d\n", first->value);
+        que->first = first->next;
+        free(first);
+        que->size--;
+    }
 }
 
 bool empty(queue* que)
@@ -70,14 +84,13 @@ int front(queue* que)
 
 int back(queue* que)
 {
-    if (que->last == NULL)
+    if (que->first == NULL)
     {
         return -1;
     }
     else
     {
-        node* tempNode = que->last;
-        return tempNode->value;
+        return que->last->value;
     }
 }
 
@@ -85,48 +98,40 @@ int main()
 {
     int N = 0;
     scanf("%d", &N);
-    queue* que = malloc(sizeof(queue));
-    char order;
+    queue que;
+    init(&que);
+    char order[6];
     int value;
     for (int i = 0; i < N; i++) {
-        scanf("%c %d", &order, &value);
-        switch (order)
+        scanf("%s", order);
+        if (!strcmp(order, "push"))
         {
-            case 'push':
-            {
-                push(que, value);
-                break;
-            }
-            case 'pop':
-            {
-                pop(que);
-                break;
-            }
-            case 'empty':
-            {
-                printf("%d", empty(que));
-                break;
-            }
-            case 'size':
-            {
-                printf("%d", size(que));
-                break;
-            }
-            case 'front':
-            {
-                printf("%d", front(que));
-                break;
-            }
-            case 'back':
-            {
-                printf("%d", back(que));
-                break;
-            }
-            default:
-            {
-                /* do nothing */
-                break;
-            }
+            scanf("%d", &value);
+            push(&que, value);
+        }
+        else if (!strcmp(order, "pop"))
+        {
+            pop(&que);
+        }
+        else if (!strcmp(order, "empty"))
+        {
+            printf("%d\n", empty(&que));
+        }
+        else if (!strcmp(order, "size"))
+        {
+            printf("%d\n", size(&que));
+        }
+        else if (!strcmp(order, "front"))
+        {
+            printf("%d\n", front(&que));
+        }
+        else if (!strcmp(order, "back"))
+        {
+            printf("%d\n", back(&que));
+        }
+        else
+        {
+
         }
     }
     return 0;
