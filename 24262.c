e09af3de0d7292047bd078e9ce68bf922ced5f26
Apprentice-CB <yewon0612@gmail.com>
2023-03-05 00:06:25 +0900

    2743, 24262~24264 solved (23.03.05)

---
+++ 24262.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    printf("%d\n0", n);
+    return 0;
+}
---
+++ 24264.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    long long n = 0;
+    scanf("%d", &n);
+    printf("%lld\n2", n*n);
+    return 0;
+}
