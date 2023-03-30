1343c35adb82492823839cf3a3f4471b51f6cc6d
Apprentice-CB <yewon0612@gmail.com>
2023-03-30 22:38:40 +0900

    solving 24479 in progress (23.03.30)

--- 24479_yet.c
+++ 24479_yet.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void dfs(int** graph, int* visited, int N, int R)
+{
+    visited[R] = 1;
+    for (int node = 0; node < N; node++)
+    {
+        if (visited[node] == 0)
+        {
+            dfs(graph, visited, N, node);
+        }
+    }
+}
 
 int main(void)
 {
@@ -8,18 +21,31 @@ int main(void)
     int R = 0;
     int u = 0;
     int v = 0;
+    int cnt = 0;
     scanf("%d %d %d", &N, &M, &R);
     int** graph;
     graph = (int**)malloc(sizeof(int*) * N);
+    int* visited;
+    visited = (int*)malloc(sizeof(int) * N);
     for (int i = 0; i < N; i++)
     {
         graph[i] = (int*)malloc(sizeof(int) * N);
+        visited[i] = 0;
     }
     memset(graph, 0, N * N * sizeof(int));
 
     for (int i = 0; i < M; i++)
     {
-        
+        scanf("%d %d", &u, &v);
+        graph[u-1][v-1] = 1;
+        graph[v-1][u-1] = 1;
     }
-
+    dfs(graph, visited, N, R-1);
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d\n", visited[i]);
+    }
+    free(graph);
+    free(visited);
+    return 0;
 }
