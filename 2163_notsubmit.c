040e8f04963b3c7059767060219621068e668db8
Apprentice-CB <yewon0612@gmail.com>
2023-04-28 19:34:22 +0900

    2163, 3046 solved, not submitted (23.04.28)

---
+++ 2163_notsubmit.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N = 0;
+    int M = 0;
+    scanf("%d %d", &N, &M);
+    printf("%d", N * M -1);
+}
---
+++ 3046_notsubmit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int R1 = 0;
+    int R2 = 0;
+    int S = 0;
+    scanf("%d %d", &R1, &S);
+    printf("%d", 2*S - R1);
+}
