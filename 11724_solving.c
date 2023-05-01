72e337e39accff31c140eebba8ff8bdfb26c18eb
Apprentice-CB <yewon0612@gmail.com>
2023-05-01 17:48:18 +0900

    solving 11724 (23.05.01)

---
+++ 11724_solving.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef struct node
+{
+    int number;
+    struct node* next;
+} node;
+
+node graph[1001];
+
+int main(void)
+{
+    int N = 0;
+    int M = 0;
+    scanf("%d %d", &N, &M);
+    for (int i = 0; i < M; i++)
+    {
+        
+    }
+}
