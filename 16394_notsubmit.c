d64b0722e9e5060d6c7b31d0a095a922dcd1def9
Apprentice-CB <yewon0612@gmail.com>
2023-05-04 08:25:58 +0900

    2420, 16394 solved, not submitted (23.05.04)

---
+++ 16394_notsubmit.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int var = 0;
+    scanf("%d", &var);
+    printf("%d", var-1946);
+}
---
+++ 2420_notsubmit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    long long N = 0;
+    long long M = 0;
+    scanf("%lld %lld", &N, &M);
+    printf("%lld", llabs(N - M));
+}
