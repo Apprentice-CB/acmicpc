b5580fcab211a4a1a1e240fff0afacf367aa9c3d
Apprentice-CB <yewon0612@gmail.com>
2023-05-03 22:56:49 +0900

    solving 11724... (23.05.03)

--- 11724_solving.c
+++ 11724_solving.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-typedef struct node
+uint8_t graph[1001][1001];
+uint8_t vertex[1001];
+uint8_t visited[1001];
+
+int DFS(uint8_t graph[][1001], int N, int start)
 {
-    int number;
-    struct node* next;
-} node;
 
-node graph[1001];
+}
 
 int main(void)
 {
     int N = 0;
     int M = 0;
     scanf("%d %d", &N, &M);
+    int u = 0;
+    int v = 0;
+    int start = 0;
     for (int i = 0; i < M; i++)
     {
-        
+        scanf("%d %d", &u, &v);
+        graph[u][v] = 1;
+        graph[v][u] = 1;
+        vertex[u] = 1;
+        vertex[v] = 1;
     }
+
+    int cnt = 0;
+    cnt += DFS(graph, N, start);
+    printf("%d", cnt);
 }
