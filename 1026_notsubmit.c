989cfef80a0459a5dbc79534385313f52be5fcaa
Apprentice-CB <yewon0612@gmail.com>
2023-05-07 19:17:10 +0900

    1026 solved, not submitted (23.05.07)

---
+++ 1026_notsubmit.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compareA(const void* a, const void* b)
+{
+    if (*(int*)a > *(int*)b)
+    {
+        return 1;
+    }
+    else if (*(int*)a < *(int*)b)
+    {
+        return -1;
+    }
+    return 0;
+}
+
+int compareB(const void* a, const void* b)
+{
+    if (*(int*)a > *(int*)b)
+    {
+        return -1;
+    }
+    else if (*(int*)a < *(int*)b)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int main(void)
+{
+    int N = 0;
+    scanf("%d", &N);
+    int* arrA = (int*)malloc(sizeof(int) * N);
+    int* arrB = (int*)malloc(sizeof(int) * N);
+    int input = 0;
+    for (int i = 0; i < N; i++)
+    {
+        scanf(" %d", &input);
+        arrA[i] = input;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        scanf(" %d", &input);
+        arrB[i] = input;
+    }
+    qsort(arrA, N, sizeof(int), compareA);
+    qsort(arrB, N, sizeof(int), compareB);
+    int sum = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ", arrA[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ", arrB[i]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < N; i++)
+    {
+        sum += arrA[i]*arrB[i];
+    }
+    printf("%d", sum);
+}
