de6a397e05a2e2e73880c63d32b24f779e48c550
Apprentice-CB <yewon0612@gmail.com>
2023-05-15 13:11:14 +0900

    solving 7576 (23.05.15)

--- 7576_solving.c
+++ 7576_solving.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct node
 {
@@ -53,7 +55,33 @@ void BFS(int** map, int N, int M, int nidx, int midx)
 {
     queue que;
     initQueue(&que);
-    
+    int** visited = (int**)malloc(sizeof(int*) * N);
+    for (int i = 0; i < N; i++)
+    {
+        visited[i] = (int*)malloc(sizeof(int) * M);
+        memset(visited[i], 0, sizeof(int) * M);
+    }
+    visited[nidx][midx] = 0;
+    enqueue(&que, nidx, midx);
+    int n_next = 0;
+    int m_next = 0;
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
+    node cur;
+    while (que.first != NULL)
+    {
+        cur = dequeue(&que);
+        for (int i = 0; i < 4; i++)
+        {
+            n_next = cur.x + dx[i];
+            m_next = cur.y + dy[i];
+            if (visited[n_next][m_next] == 0 && map[n_next][m_next] == 0)
+            {
+                enqueue(&que, n_next, m_next);
+                visited[n_next][m_next] = 1;
+            }
+        }
+    }
 }
 
 int main(void)
@@ -76,6 +104,25 @@ int main(void)
     }
 
     int** map_cpy = (int**)malloc(sizeof(int*) * N);
-    
+    for (int i = 0; i < N; i++)
+    {
+        map_cpy[i] = (int*)malloc(sizeof(int) * M);
+        memcpy(map_cpy[i], map[i], sizeof(int) * M);
+    }
 
+    for (int nidx = 0; nidx < N; nidx++)
+    {
+        for (int midx = 0; midx < M; midx++)
+        {
+            if (map_cpy[nidx][midx] == 1)
+            {
+                BFS(map, N, M, nidx, midx);
+            }
+        }
+    }
+
+    for (int nidx = 0; nidx < N; nidx++)
+    {
+        for (int m)
+    }
 }
