6c243dd990a2f894d34b068d817907a5f8ee6af3
Apprentice-CB <yewon0612@gmail.com>
2023-06-06 16:19:43 +0900

    4470 submitted (23.06.06)

--- 4470.c
+++ 4470.c
@@ -2,12 +2,14 @@
 
 int main(void)
 {
-    char in[51];
+    char in[1000];
     int N = 0;
     scanf("%d", &N);
     for (int i = 1; i <= N; i++)
     {
+        getchar();
         scanf(" %[^\n]", in);
         printf("%d. %s\n", i, in);
     }
+    return 0;
 }
