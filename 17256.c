29dcf6b6ad30bad584493c96bc208d6000770eb4
Apprentice-CB <yewon0612@gmail.com>
2023-10-31 19:44:39 +0900

    17256 solved (23.10.31_

---
+++ 17256.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int ax, ay, az, bx, by, bz, cx, cy, cz;
+    scanf("%d %d %d", &ax, &ay, &az);
+    scanf("%d %d %d", &cx, &cy, &cz);
+    bx = cx - az;
+    by = cy / ay;
+    bz = cz - ax;
+    printf("%d %d %d", bx, by, bz);
+    return 0;
+}
